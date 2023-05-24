e15673f13548590f7f27d4de92183d5dbbfb333d
johnma <ma.jonathan02@gmail.com>
2023-05-24 16:46:00 -0400

    feat: removed MongoDB to clean up project for k8s implementation

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, send_from_directory, redirect, url_for, send_f
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS, cross_origin 
 from convexhull import graham_scan, Animator
-from pymongo import MongoClient
 import json
 import sys
 import numpy as np
@@ -11,40 +10,29 @@ app = Flask(__name__, static_url_path='/', static_folder='frontend/build')
 CORS(app)
 api = Api(app)
 
-#mongoClient = MongoClient('mongodb://127.0.0.1:27017')
-mongoClient = MongoClient('mongodb:27017', username="root", password="rootpassword")
-db = mongoClient.get_database('submissions_db')
-submissions_col = db.get_collection('submissions_col')
- 
+def update_gif(sub):
+    pts = list(map(lambda x : float(x), sub.split()))
 
-def update_gif(id):
-    if submissions_col.find({"_id":id}):
-        pts = list(map(lambda x : float(x), submissions_col.find_one({"_id":id})['vals'].split()))
-    
-        cartesian = []
+    cartesian = []
 
-        i = 0
-        while(i < len(pts)):
-            cartesian += [[pts[i], pts[i+1]]]
-            i+=2
-        processed_pts = graham_scan(np.unique(np.array(cartesian), axis=0))
-        create_gif = Animator(processed_pts)
-        create_gif.animate()
-        return 
+    i = 0
+    while(i < len(pts)):
+        cartesian += [[pts[i], pts[i+1]]]
+        i+=2
+    processed_pts = graham_scan(np.unique(np.array(cartesian), axis=0))
+    create_gif = Animator(processed_pts)
+    create_gif.animate()
+    return 
 
 @app.route('/api/addsubmission', methods=['POST'])
 def addsubmission():
     data = request.get_json()
-
     sub = data['vals']
-    
-    result = submissions_col.insert_one({"vals": sub})
-    update_gif(result.inserted_id)
+    update_gif(sub)
 
     return send_file("gscan.gif", mimetype="image/gif")
-  # Save the item to the database
 
-@app.route("/api/profile")
+@app.route("/api/test")
 def app_profile():
     return {"Systems": "Nominal"}
 
